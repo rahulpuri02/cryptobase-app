8d6bf2f12007dccce56ada5c751486f0f07f7dae
Rahul Puri <rahulxpuri@gmail.com>
2022-10-23 18:27:48 +0530

    display the data on table component

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { CryptoContext } from '../context/CryptoContext'
-
+import {ImStarEmpty} from 'react-icons/im'
 function TableComponent() {
 
      let {cryptoData} = useContext(CryptoContext);
@@ -33,14 +33,24 @@ function TableComponent() {
         cryptoData.map(data => {
             return (
                 <tr key={data.id} className='text-center text-base border-b border-gray-100 hover:bg-gray-200 last:border-b-0'>
-                <td className='py-4'>Assets</td>
+                <td className='py-4 flex items-center uppercase'>
+                  <button className='cursor-pointer'><ImStarEmpty className='w-6 h-6 ml-[5px]'/></button>
+                  <img className='w-6 h-6 mx-3' src={data?.image} alt="{data.name}" />
+                  <span>{data.symbol}</span>
+                </td>
                     <td className='py-4'>{data.name}</td>
-                    <td className='py-4'>Price</td>
-                    <td className='py-4'>Total Volume</td>
-                    <td className='py-4'>Mkt Cap Change</td>
-                    <td className='py-4'>1H</td>
-                    <td className='py-4'>24HR</td>
-                    <td className='py-4'>7D</td>
+                    <td className='py-4'>{data.current_price}</td>
+                    <td className='py-4'>{data.total_volume}</td>
+                    <td className='py-4'>{data.market_cap_change_percentage_24h}%</td>
+                    <td className={
+                      data.price_change_percentage_1h_in_currency > 0 ? 'text-green py-4' : 'text-red py-4'
+                    }>{Number(data.price_change_percentage_1h_in_currency).toFixed(2)}</td>
+                    <td className={
+                      data.price_change_percentage_1h_in_currency > 0 ? 'text-green py-4' : 'text-red py-4'
+                    }>{Number(data.price_change_percentage_24h_in_currency).toFixed(2)}</td>
+                  <td className={
+                      data.price_change_percentage_1h_in_currency > 0 ? 'text-green py-4' : 'text-red py-4'
+                    }>{Number(data.price_change_percentage_7d_in_currency).toFixed(2)}</td>
                 </tr>
             )
         })
--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -13,7 +13,7 @@ export const CryptoProvider = ({children}) => {
     try {
         const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`)
         .then(res => res.json());
-        console.log(data);
+       console.log(data);
         setCryptoData(data);
 
     }catch(error){
