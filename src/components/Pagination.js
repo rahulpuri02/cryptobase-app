64f1d8df232341be5c297389ec3539585f687ffe
Rahul Puri <rahulxpuri@gmail.com>
2022-10-25 07:07:12 +0530

    completed the pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,41 +1,43 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import paginationArrow from "../assets/pagination-arrow.svg";
+import { CryptoContext } from '../context/CryptoContext';
 
 function Pagination() {
-
-    const[currentPage, setCurrentPage] = useState(1);
     
-    let TotalNumber = 1350;
+
+   let {page, setPage, totalPages} = useContext(CryptoContext);
+   
+   const TotalNumber = Math.ceil(totalPages/10);
 
     const nextPage = () => {
-    if(currentPage === TotalNumber){
+    if(page === TotalNumber){
         return null;
     }else {
-        setCurrentPage(currentPage + 1);
+        setPage(page + 1);
     }
     }
 
     const prevPage = () => {
-        if(currentPage === 1){
+        if(page === 1){
             return null;
         }else {
-            setCurrentPage(currentPage - 1);
+            setPage(page - 1);
         }
         }
 
    const multiStepNext = () =>{
-    if(currentPage+3 >= TotalNumber){
-        setCurrentPage(TotalNumber - 1)
+    if(page+3 >= TotalNumber){
+        setPage(TotalNumber - 1)
     }else{
-        setCurrentPage(currentPage + 3)
+        setPage(page + 3)
     }
    }
 
    const multiStepPrev = () =>{
-    if(currentPage - 3 <= 1){
-        setCurrentPage(TotalNumber + 1)
+    if(page - 3 <= 1){
+        setPage(TotalNumber + 1)
     }else{
-        setCurrentPage(currentPage - 2)
+        setPage(page - 2)
     }
    }
 
@@ -43,23 +45,27 @@ function Pagination() {
   return (
     <div className='flex items-center'>
         <ul className='flex items-center justify-end text-sm'>
-        <li className='flex items-center'>
-            <button  onClick={prevPage} className='outline-0 hover:w-8'>
-        <img className='w-full h-auto rotate-180' src={paginationArrow} alt="arrow-right" /> 
-        </button></li>
-        <li><button onClick={multiStepPrev} className='outline-0 rounded-full w-9 h-9 flex items-center justify-center text-lg'>...</button></li>
-        <li><button className='outline-0 rounded-full w-9 h-9 flex items-center justify-center bg-gray-200 mx-1.5'>{currentPage - 1 }</button></li>
-        <li><button className='outline-0 rounded-full w-9 h-9 flex items-center justify-center text-gray-300 bg-blue mx-1.5'>{currentPage }</button></li>
+       <li className='flex items-center'>
+        <button  onClick={prevPage} className='outline-0 hover:w-8'>
+    <img className='w-full h-auto rotate-180' src={paginationArrow} alt="arrow-right" /> 
+     </button></li> 
+     {
+        (page + 1 === TotalNumber || page === TotalNumber) ? <li><button onClick={multiStepPrev} className='outline-0 rounded-full w-9 h-9 flex items-center justify-center text-lg'>...</button></li> : null
+     } 
+      {
+         (page -1 !== 0) ? <li><button onClick={prevPage} className='outline-0 rounded-full w-9 h-9 flex items-center justify-center bg-gray-200 mx-1.5'>{page - 1 }</button></li>: null
+        }
+        <li><button className='outline-0 rounded-full w-9 h-9 flex items-center justify-center text-gray-300 bg-blue mx-1.5'>{page }</button></li>
 
        {
-        (currentPage +1 !== TotalNumber && currentPage !== TotalNumber) ? 
-        <li ><button className='outline-0 rounded-full w-9 h-9 flex items-center justify-center bg-gray-200 mx-1.5'>{currentPage + 1 }</button></li> : null
+        (page +1 !== TotalNumber && page !== TotalNumber) ? 
+        <li ><button onClick={nextPage} className='outline-0 rounded-full w-9 h-9 flex items-center justify-center bg-gray-200 mx-1.5'>{page + 1 }</button></li> : null
        }
        {
-        currentPage +1 !== TotalNumber && currentPage !== TotalNumber ?  <li><button onClick={multiStepNext} className='outline-0 rounded-full w-9 h-9 flex items-center justify-center text-lg'>...</button></li> : null
+        page +1 !== TotalNumber && page !== TotalNumber ?  <li><button onClick={multiStepNext} className='outline-0 rounded-full w-9 h-9 flex items-center justify-center text-lg'>...</button></li> : null
        }
        {
-        currentPage !== TotalNumber ? <li ><button onClick={() => setCurrentPage(TotalNumber)} className='outline-0 rounded-full w-9 h-9 flex items-center justify-center bg-gray-200 mx-1.5'>{TotalNumber}</button></li> : null
+        page !== TotalNumber ? <li ><button onClick={() => setPage(TotalNumber)} className='outline-0 rounded-full w-9 h-9 flex items-center justify-center bg-gray-200 mx-1.5'>{TotalNumber}</button></li> : null
        }
        <li>
         <button  onClick={nextPage} className='ml-1.5 flex items-center'>
--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -9,11 +9,22 @@ const[searchData, setSearchData] = useState();
 const[coinSearch, setCoinSearch] = useState("");
 const[currency, setCurrency] = useState("usd");
 const[sortBy, setSortBy] = useState("market_cap_desc")
+const[page, setPage] = useState(1);
+const [totalPages, setTotalPages] = useState(50);
 
     const getCryptoData = async () => {
      
       try {
-          const coin = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`)
+        const coinList = await fetch(`https://api.coingecko.com/api/v3/coins/list`)
+        .then(res => res.json());
+        setTotalPages(coinList.length);
+
+    }catch(error){
+        console.log(error);
+    }
+
+      try {
+          const coin = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=${page}&sparkline=true&price_change_percentage=1h%2C24h%2C7d`)
           .then(res => res.json());
           setCryptoData(coin);
   
@@ -21,6 +32,7 @@ const[sortBy, setSortBy] = useState("market_cap_desc")
           console.log(error);
       }
     };
+  
   
 
 const getSearchResult= async (query) => {
@@ -39,7 +51,7 @@ const getSearchResult= async (query) => {
 
 useLayoutEffect(() => {
   getCryptoData();
-},[coinSearch, currency, sortBy])
+},[coinSearch, currency, sortBy, page])
  
   
   return (
@@ -52,7 +64,10 @@ useLayoutEffect(() => {
           currency, 
           setCurrency,
           sortBy,
-          setSortBy}}>
+          setSortBy,
+          page,
+          setPage,
+          totalPages}}>
             {children}
         </CryptoContext.Provider>
     )
